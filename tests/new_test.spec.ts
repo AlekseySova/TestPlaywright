d8a9c17515e4c51224b6e7d29eaa84dea86265d9
AlekseySova <citigacy@gmail.com>
2025-04-06 10:14:37 +0300

    Add BeforeEach and AfterEach annotations. Create test for typescript page

--- tests/new_test.spec.ts
+++ tests/new_test.spec.ts
@@ -1,10 +1,31 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+
+test.beforeEach(async ({ page }) => {
+
     await page.goto('https://www.w3schools.com/');
+});
+
+test.afterEach(async ({ page }) => {
+    
+    await page.close();
+  });
+
+test('Has proper URL', async ({ page }) => {
   
     await page.locator(".ga-nav[title='Java Tutorial']").click();
 
     
     await expect(page).toHaveURL("https://www.w3schools.com/java/default.asp");
+
   });
+
+test('Search for typescript tutorial', async ({ page }) => {
+
+    await page.locator('#tnb-google-search-input').fill('typescript');
+
+    await page.locator('#tnb-google-search-input').press('Enter');
+
+    await expect(page).toHaveURL('https://www.w3schools.com/typescript/index.php');
+
+})
