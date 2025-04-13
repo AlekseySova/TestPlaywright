0d084527b7ba1e456e8993d07324b9953c67945e
AlekseySova <citigacy@gmail.com>
2025-04-13 12:53:05 +0300

    Add test with placeholder locator

--- tests/playwrightLocators.spec.ts
+++ tests/playwrightLocators.spec.ts
@@ -26,4 +26,21 @@ test.afterEach(async ({ page }) => {
 
     await page.getByLabel('Search our tutorials').press('Enter');
 
+
+  });
+
+  test("Get by Placeholder locator", async ({ page }) => {
+
+    await page.getByPlaceholder('Search our tutorials, e.g. HTML').fill('BOOTSTRAP');
+
+    await page.getByPlaceholder('Search our tutorials, e.g. HTML').press('Enter');
+
+    await expect(page).toHaveURL('https://www.w3schools.com/bootstrap/default.asp');
+
+    await expect(page).toHaveTitle('Bootstrap 3 Tutorial');
+
+    await expect(page.locator('#main > h1')).toHaveText('Bootstrap 3 Tutorial');
+
+
+
   });
