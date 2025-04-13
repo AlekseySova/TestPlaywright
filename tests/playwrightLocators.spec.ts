d6a198e43eec2dcaab44e43974e9f43d2ab37303
AlekseySova <citigacy@gmail.com>
2025-04-13 09:32:53 +0300

    Add test with getByRole locator

---
+++ tests/playwrightLocators.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+
+    await page.goto('https://www.w3schools.com/');
+});
+
+test.afterEach(async ({ page }) => {
+    
+    await page.close();
+  });
+
+  test('Role locator', async ({ page }) => {
+
+    await page.getByRole('button', { name: 'Services' }).click();
+
+    await page.getByRole('heading', {name: 'Free Tutorials'}).click();
+
+    await expect(page.getByRole('heading', { name: 'An Overview of All Tutorials' })).toHaveText('An Overview of All Tutorials');
+
+  });
